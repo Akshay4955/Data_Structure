486e33e8d1819590b9e06901e485a70f4e6fb3fe
Akshay4955 <akshayshedge11@gmail.com>
2023-05-22 16:40:57 +0530

    Added: Code for add first to linked list

--- Data_Structures/Program.cs
+++ Data_Structures/Program.cs
@@ -1,10 +1,77 @@
-﻿namespace Data_Structures
+﻿using System.Text;
+
+namespace Data_Structures
 {
+    public class Node<T>
+    {
+        public T Key
+        { get; set; }
+        public Node<T> Next
+        { get; set; }
+
+        public Node(T key)
+        {
+            Key = key;
+        }
+    }
+
+    public class MyLinkedList<T>
+    {
+        public Node<T> head;
+        public Node<T> tail;
+
+        public MyLinkedList() 
+        {
+            head = null;
+            tail = null;
+        }
+
+        public void addFirst(Node<T> node)
+        {
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+                Node<T> tempNode = head;
+                head = node;
+                head.Next = tempNode;
+            }
+        }
+
+        public void printLikedList()
+        {
+            StringBuilder myLinkedList = new StringBuilder();
+            Node<T> tempNode = head;
+            while (tempNode.Next != null) 
+            {
+                myLinkedList.Append(tempNode.Key);
+                if (!tempNode.Equals(tail))
+                    myLinkedList.Append("->");
+                tempNode = tempNode.Next;
+            }
+            myLinkedList.Append(tempNode.Key);
+            Console.WriteLine(myLinkedList);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to data structures...!!!");
+            Node<int> myFirstNode = new Node<int>(70);
+            Node<int> mySecondNode = new Node<int>(40);
+            Node<int> myThirdNode = new Node<int>(30);
+            
+            MyLinkedList<int> myLinkedList = new MyLinkedList<int>();
+            myLinkedList.addFirst(myFirstNode);
+            myLinkedList.addFirst(mySecondNode);
+            myLinkedList.addFirst(myThirdNode);
+
+            myLinkedList.printLikedList();
+
         }
     }
 }
